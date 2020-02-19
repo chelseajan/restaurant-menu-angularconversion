a5b482cb96d0aa7d9c41fe411dd579d84c45f1cd
chelsealjan <chelsealjan@gmail.com>
2020-02-18 17:01:06 -0800

    Defined all menu items in array

---
+++ src/app/Items.ts
@@ -0,0 +1,41 @@
+import { Item } from './item';
+
+export const ITEMS: Item[] = [
+    {
+        type: "appetizer",
+        name: "Gyoza",
+        price: 5.00,
+        description: "Fried dumplings (6ps) stuffed with pork, chicken, and cabbage. Served with ponzu sauce."
+    },
+    {
+        type: "appetizer",
+        name: "Karaage",
+        price: 5.00,
+        description: "House marinated and fried chicken (6ps). Served with lemon."
+    },
+    {
+        type: "appetizer",
+        name: "Edamame",
+        price: 2.95,
+        description: "Boiled green soybeans with salt."
+    },
+
+    {
+        type: "entree",
+        name: "Tonkotsu Ramen",
+        price: 10.95,
+        description: "Special housemade pork broth. <br>Served with 1 egg, pork chashu, bean sprouts, green onions, mushrooms, and roasted sesame seeds."
+    },
+    {
+        type: "entree",
+        name: "Miso Ramen",
+        price: 12.45,
+        description: "Special housemade pork broth flavored with housemade miso. Served with 1 egg, pork chashu, bean sprouts, green onions, mushrooms, ginger, and roasted sesame seeds."
+    },
+    {
+        type: "entree",
+        name: "Tan Tan Ramen",
+        price: 11.45,
+        description: "Special housemade pork broth flavored with housemade miso, chili oil, and sesame paste. Served with your choice of chicken or pork cutlet, seasoned minced pork, bean sprouts, green onions, and roasted sesame seeds."
+    }
+  ];
